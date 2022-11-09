65de49b8a785e3414fa1c06be26b5c189aaef6e0
Xiang Lu <xil621@ucsd.edu>
2022-11-09 15:55:15 -0800

    draft cache func signatures; lighter CacheConfig

--- cache.cpp
+++ cache.cpp
@@ -8,6 +8,9 @@
 
 using namespace::std;
 
+namespace MemoryCacheSim
+{
+
 RWObject::RWObject(char* data): _data(data) {
     if (sizeof(data) == 0){
         throw std::invalid_argument("data size cannot be empty");
@@ -19,6 +22,19 @@ RWObject::RWObject(char* data): _data(data) {
 }
 
 RWObject::~RWObject() {}
+
+
+Cache::Cache(char* cacheData, CacheConfig* config, RWObject* lowerRW) : RWObject(cacheData) {
+    setConfig(config);
+    this->_lower = lowerRW;
+}
+
+void Cache::setConfig(CacheConfig* config) {
+    this->_config = config;
+    this->_metaData = (struct MetaRow*)malloc(this->_config->_numBlocks * sizeof(struct MetaRow));
+}
+
+Cache::~Cache() {}
 
 CacheConfig::CacheConfig(
         int memoryAddrLen,
@@ -42,35 +58,62 @@ CacheConfig::CacheConfig(
 
 CacheConfig::~CacheConfig() {}
 
-Cache::Cache(char* cacheData, CacheConfig* config, RWObject* lowerRW) : RWObject(cacheData) {
-    setConfig(config);
-    this->_lower = lowerRW;
+//--------------------------------------------------
+// Implementation of the cache replacement mechnisms
+//--------------------------------------------------
+void Cache::LRUCacheReplacement() {
 }
 
-void Cache::setConfig(CacheConfig* config) {
-    this->_config = config;
-    this->_metaData = (struct MetaRow*)malloc(this->_config->_numBlocks * sizeof(struct MetaRow));
+void Cache::FIFOCacheReplacement() {
 }
 
-Cache::~Cache() {}
+void Cache::ClockCacheReplacement() {
+}
 
-size_t CacheConfig::parseTag(const unsigned long &address) {
+//--------------------------------------------------
+// Implementation of cache miss handling
+//--------------------------------------------------
+void Cache::handleReadMiss() {
+     _miss_read_count += 1;
+    this->_lower->read(dest, address);
+}
+
+void Cache::handleWriteMiss() {}
+
+//--------------------------------------------------
+// Implementation of cache mishits handling
+//--------------------------------------------------
+void Cache::handleReadHit() {
+    // Read Hit
+    // read blocksize - offset
+    size_t cacheOffset = this->_config->parseBlockOffset(address);
+    size_t readSize = this->_config->_blockSize - cacheOffset;
+    memcpy(dest, cacheBlock + cacheOffset, readSize);
+}
+
+void Cache::handleWriteHit() {}
+
+
+//--------------------------------------------------
+// Implementation of parsing
+//--------------------------------------------------
+char* Cache::getData() {
+    return this->_data;
+}
+
+size_t Cache::parseTag(const unsigned long &address) {
     unsigned long tag = address >> (this->_numBitSetIndex + this->_numBitBlockOffset);
     return size_t(tag);
 }
 
-size_t CacheConfig::parseSetIndex(const unsigned long &address) {
+size_t Cache::parseSetIndex(const unsigned long &address) {
     unsigned long shifted = address >> (this->_numBitBlockOffset);
     unsigned long setIndex = shifted & int(pow(2, this->_numBitSetIndex) - 1);
     return size_t(setIndex);
 }
 
-size_t CacheConfig::parseBlockOffset(const unsigned long &address) {
+size_t Cache::parseBlockOffset(const unsigned long &address) {
     return address & int(pow(2, this->_numBitBlockOffset) - 1);
-}
-
-char* RWObject::getData() {
-    return this->_data;
 }
 
 char* Cache::getCacheBlock(size_t targetTag, size_t targetSetIndex) {
@@ -85,42 +128,27 @@ char* Cache::getCacheBlock(size_t targetTag, size_t targetSetIndex) {
     return nullptr;
 }
 
+
+//--------------------------------------------------
+// Implementation of cache read and write
+//--------------------------------------------------
 void Cache::read(char* dest, const unsigned long &address) {
-    size_t tag = this->_config->parseTag(address);
-    size_t setIndex = this->_config->parseSetIndex(address);
+    size_t tag = parseTag(address);
+    size_t setIndex = parseSetIndex(address);
 
     char* cacheBlock = getCacheBlock(tag, setIndex);
     _read_count += 1;
 
-    // Read Miss
-    if (cacheBlock == nullptr) {
-        _miss_read_count += 1;
-        this->_lower->read(dest, address);
+    if (cacheBlock == nullptr) { // Read Miss
+        handleReadMiss();
         return;
     }
-    // Read Hit
-    // read blocksize - offset
-    size_t cacheOffset = this->_config->parseBlockOffset(address);
-    size_t readSize = this->_config->_blockSize - cacheOffset;
-    memcpy(dest, cacheBlock + cacheOffset, readSize);
+    handleReadHit();
+    return;
 }
 
 void Cache::write(char* src, const unsigned long &address) {
     
 }
 
-Memory::~Memory() {}
-
-Memory::Memory(char* memoryData): RWObject(memoryData) {
-
-}
-
-void Memory::read(char* dest, const unsigned long &address) {
-    char* data = this->getData();
-    memcpy(dest, &(data[address]), sizeof(dest));
-}
-
-void Memory::write(char* src, const unsigned long &address) {
-    char* data = this->getData();
-    memcpy(&(data[address]), src, sizeof(src));
 }
